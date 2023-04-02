d8b4fa6f3034d5fb6058552215a5c5c41716b3ed
MatthieuFra <93255376+MatthieuFra@users.noreply.github.com>
2023-04-02 19:48:43 +0200

    add parameters in config file

---
+++ heart-voice/js/config.js
@@ -0,0 +1,26 @@
+/*
+
+EXPERIMENT CONFIGURATION FILE: 
+
+Here's the configuration file, to modify the parameters of the experiment.
+
+*/
+
+const VIDEOS = [
+  "vid/TEST2.mp4",
+  "vid/TEST3.mp4",
+  "vid/TEST2.mp4",
+  "vid/TEST3.mp4",
+  ];
+
+const VIDEOS_TRAINING = [
+  "vid/TEST2.mp4",
+  "vid/TEST2.mp4",
+  ];
+
+const CONSIGNE1 = `INSERER CONSIGNES 1`;
+const CONSIGNE2 = `INSERER CONSIGNES 2`;
+const CONSIGNE3 = `INSERER CONSIGNES 3`;
+//const CONSIGNE1 = ``;
+
+const EMAIL = "example@mail.com";

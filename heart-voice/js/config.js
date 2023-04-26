bab0c0b43bbbdecb34799dc48c289f58c3274d47
MatthieuFra <93255376+MatthieuFra@users.noreply.github.com>
2023-04-26 17:20:10 +0200

    Maj expe : add block ABC with counterbalancing and change format of videos and change data

--- heart-voice/js/config.js
+++ heart-voice/js/config.js
@@ -6,21 +6,66 @@ Here's the configuration file, to modify the parameters of the experiment.
 
 */
 
-const VIDEOS = [
+
+// the participant number (this affects the order of presentation of the ABC blocks)
+const PARTICIPANT = 10;
+
+
+// the videos used, for each part of the experiment
+
+/*
+const VIDEOS1 = [
   "vid/TEST2.mp4",
   "vid/TEST3.mp4",
   "vid/TEST2.mp4",
-  "vid/TEST3.mp4",
   ];
 
+const VIDEOS2 = [
+  "vid/TEST2.mp4",
+  "vid/TEST3.mp4",
+  "vid/TEST2.mp4",
+  ];
+
+const VIDEOS3 = [
+  "vid/TEST2.mp4",
+  "vid/TEST3.mp4",
+  "vid/TEST2.mp4",
+  ];
+
+*/
+
+// pairs of video for first part of the experiment
+const VIDEOS1 = [
+  ["vid/TEST2.mp4", "vid/TEST3.mp4"],
+  ["vid/TEST2.mp4", "vid/TEST3.mp4"],
+  ["vid/TEST2.mp4", "vid/TEST3.mp4"],
+];
+
+// pairs of video for second part of the experiment
+const VIDEOS2 = [
+  ["vid/TEST2.mp4", "vid/TEST3.mp4"],
+  ["vid/TEST2.mp4", "vid/TEST3.mp4"],
+  ["vid/TEST2.mp4", "vid/TEST3.mp4"],
+];
+
+// pairs of video for third part of the experiment
+const VIDEOS3 = [
+  ["vid/TEST2.mp4", "vid/TEST3.mp4"],
+  ["vid/TEST2.mp4", "vid/TEST3.mp4"],
+  ["vid/TEST2.mp4", "vid/TEST3.mp4"],
+];
+
+
+// videos of the training phase, only one pair is required
 const VIDEOS_TRAINING = [
   "vid/TEST2.mp4",
   "vid/TEST2.mp4",
   ];
 
+// here to modify the instructions
 const CONSIGNE1 = `INSERER CONSIGNES 1`;
 const CONSIGNE2 = `INSERER CONSIGNES 2`;
 const CONSIGNE3 = `INSERER CONSIGNES 3`;
-//const CONSIGNE1 = ``;
 
+// email used to contact the research team if needed
 const EMAIL = "example@mail.com";

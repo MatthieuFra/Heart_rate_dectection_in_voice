75870f5c4453c15b9d02b3742b461e74c86a152f
MatthieuFra <93255376+MatthieuFra@users.noreply.github.com>
2023-05-03 10:12:27 +0200

    DEBUG change data saving, add debugged video, change data

--- heart-voice/js/config.js
+++ heart-voice/js/config.js
@@ -7,9 +7,6 @@ Here's the configuration file, to modify the parameters of the experiment.
 */
 
 
-// the participant number (this affects the order of presentation of the ABC blocks)
-const PARTICIPANT = 10;
-
 
 // here to modify the instructions
 const CONSIGNE1 = `INSERER CONSIGNES 1`;
